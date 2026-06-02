73217f27d1c2a277efc802c8f635e41be47949ca
Santiago915-RGB <s99503294@gmail.com>
2026-06-02 14:21:10 -0600

    dwl config

--- commands/start_dwl.sh
+++ commands/start_dwl.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-dwl -s "sh -c '~/.commands/hubData.sh & swaybg -o HDMI-A-1 -i /home/pachas/downloads/gentoowp-0.png -o eDP-1 -i /home/pachas/downloads/gentoowp-1.png & dbus-launcher & gentoo-pipewire-launcher & ibus start &'"
+dwl -s "sh -c '~/.commands/hubData.sh & swaybg -o HDMI-A-1 -i /home/user/downloads/gentoowp-0.png -o eDP-1 -i /home/user/downloads/gentoowp-1.png & dbus-launcher & gentoo-pipewire-launcher & ibus start &'"
